d72f9d1c1003b00bec1e60dece19c70196ec3e40
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2023-11-07 21:23:52 +0200

    fix live

--- frontend/src/components/Live.js
+++ frontend/src/components/Live.js
@@ -8,26 +8,25 @@ import LogoLoader from "./LogoLoader";
 
 const Live = () => {
     const {youtubeLink, liveDescription, liveDescriptionBg, language} = useContext(AppContext);
-    const [isLoading, setIsLoading] = useState(true);
-    // console.log(liveDescriptionBg)
     const parser = new DOMParser();
     const doc = parser.parseFromString(youtubeLink, 'text/html');
     const iframeElement = doc.getElementsByTagName('iframe')[0];
-    const handleItemLoad = () => {
-        setIsLoading(false);
-    };
-
-
-    console.log(youtubeLink)
     return (
         <div className={'base_container'}>
             {!iframeElement ? <LogoLoader/> :
 
-                <div style={{height: '40rem', width: '80%', display:'flex', alignItems:'center', justifyContent:'center', flexWrap:'wrap'}}>
+                <div style={{
+                    height: '40rem',
+                    width: '80%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexWrap: 'wrap'
+                }}>
                     <h4>{language === 'en' ? liveDescription : liveDescriptionBg}</h4>
                     <YouTubeIframeComponent
                         style={{height: '100%'}}
-                        handleItemLoad={handleItemLoad}
+
                         width={'100%'}
                         height={'80%'}
                         src={iframeElement.src}

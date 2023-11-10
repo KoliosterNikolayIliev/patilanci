1f8d7eddcb0ccf06f9935f57b2376cc666a454d1
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2023-11-11 01:53:46 +0200

    add procfile and migrations

--- patilanci/settings.py
+++ patilanci/settings.py
@@ -168,8 +168,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # TODO
-# proc file
-# - add run build
 # test heroku
 # migrate db to heroku
 # domain

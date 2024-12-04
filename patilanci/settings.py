cdc2c8fc12bcf676930be8c5e374862562524860
KoliosterNikolayIliev <nikollay.illiev@gmail.com>
2024-12-05 01:32:58 +0200

    - wip - equalize with dev

--- patilanci/settings.py
+++ patilanci/settings.py
@@ -151,15 +151,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# TODO
-# fix settings
-# .env
-# django secret key
-# proc file
-# - add run build
-# move to main
-# test heroku
-# migrate db to heroku
-# domain
